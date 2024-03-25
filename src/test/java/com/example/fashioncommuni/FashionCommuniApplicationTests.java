530cd29e8b8abd6a3b0dd7dc2d82f8ccbe7a649a
dyk000825 <kdy000825@naver.com>
2024-03-25 18:38:47 +0900

    Initial Commit

---
+++ src/test/java/com/example/fashioncommuni/FashionCommuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fashioncommuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionCommuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
