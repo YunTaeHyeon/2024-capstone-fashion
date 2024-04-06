ebe44f91c3b7f8735de70629d700f1d4a08c5004
YunTaeHyeon <t05h19@naver.com>
2024-04-07 01:11:32 +0900

    fix: findByUserId로 변경

--- src/main/java/com/example/fashioncommuni/recommend/repository/UserCategoryScoresRepository.java
+++ src/main/java/com/example/fashioncommuni/recommend/repository/UserCategoryScoresRepository.java
@@ -3,6 +3,7 @@ package com.example.fashioncommuni.recommend.repository;
 import com.example.fashioncommuni.recommend.domain.UserCategoryScores;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserCategoryScoresRepository extends JpaRepository<UserCategoryScores, Long> {
