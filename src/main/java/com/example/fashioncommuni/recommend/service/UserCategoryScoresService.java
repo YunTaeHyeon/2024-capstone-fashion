5bd06896bb7edf282e7f054e7e8efa29fc92a406
YunTaeHyeon <t05h19@naver.com>
2024-04-07 01:14:17 +0900

    feat: 자동 csv 파일 생성 메소드 추가

--- src/main/java/com/example/fashioncommuni/recommend/service/UserCategoryScoresService.java
+++ src/main/java/com/example/fashioncommuni/recommend/service/UserCategoryScoresService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface UserCategoryScoresService {
+    void writeCsvUserCategoryScores();
     List<Double> calculateUserCategoryScores(Long userId);
     // 게시물을 조회했을 때 카테고리 스코어를 스케쥴링으로 계산하는 메서드
 }
