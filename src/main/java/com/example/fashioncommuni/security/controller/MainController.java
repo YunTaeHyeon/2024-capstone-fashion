362986ae5dd636b02a1a438571a2a5e3fffcd0ec
YunTaeHyeon <t05h19@naver.com>
2024-04-14 23:24:08 +0900

    feat: 루트페이지에서 유저카테고리 스코어가 없을 경우 초기 추천을 위한 화면 넘어가도록 로직 설정

--- src/main/java/com/example/fashioncommuni/security/controller/MainController.java
+++ src/main/java/com/example/fashioncommuni/security/controller/MainController.java
@@ -1,6 +1,14 @@
 package com.example.fashioncommuni.security.controller;
 
+import com.example.fashioncommuni.member.domain.User;
+import com.example.fashioncommuni.member.dto.SecurityUserDetailsDto;
+import com.example.fashioncommuni.member.service.SecurityUserService;
+import com.example.fashioncommuni.recommend.domain.UserCategoryScores;
+import com.example.fashioncommuni.recommend.service.UserCategoryScoresService;
+import com.example.fashioncommuni.recommend.service.UserInitInterestService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +16,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/main")
 @RequiredArgsConstructor
 @Controller
+@Slf4j
 public class MainController {
+    private final SecurityUserService securityUserService;
+    private final UserCategoryScoresService userCategoryScoresService;
 
     @GetMapping("/rootPage")
-    public String rootPage() {
+    public String rootPage(Authentication authentication) {
 
+        SecurityUserDetailsDto userDetailsDto = (SecurityUserDetailsDto) authentication.getPrincipal();
+        String authLoginId = userDetailsDto.getUsername();
+
+        System.out.println("authLoginId = " + authLoginId);
+
+        User user = securityUserService.findByLoginId(authLoginId).orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
+
+        try {
+            UserCategoryScores userCategoryScores = userCategoryScoresService.findUserCategoryScoresByUserId(user.getId());
+        } catch (Exception e) {
+            log.info("초기 추천화면");
+            return "init-interest";
+        }
+
+        log.info("초기가 아닌 화면");
         return "rootPage";
     }
 }
