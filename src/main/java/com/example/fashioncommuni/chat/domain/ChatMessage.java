f2d56d0c227e1690d349b49959573b613a91d20d
dyk000825 <kdy000825@naver.com>
2024-05-15 01:05:02 +0900

    fix: chat domain fix

--- src/main/java/com/example/fashioncommuni/chat/domain/ChatMessage.java
+++ src/main/java/com/example/fashioncommuni/chat/domain/ChatMessage.java
@@ -18,14 +18,8 @@ public class ChatMessage {
     private Long senderId;
     private String content;
 
-    // ZonedDateTime 값을 String으로 변환하여 저장
-    // ZonedDateTime을 사용하면 특정 시간대에서의 시간을 정확히 표현할 수 있다.
-    // 예를 들어, 사용자가 다른 시간대에서 메시지를 보냈다면, ZonedDateTime을 사용해 메시지를 보낸 시간을 해당 시간대의 시간으로 정확하게 표시할 수 있다.
     private String createdAt;
-    private int readCount;
 
     private ChatType chatType; // 채팅 타입 필드 추가('TEXT', 'IMAGE')
-
-    private String imageName;
     private String imageUrl;
 }
