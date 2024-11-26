c595f65522585e2332cbc2dab0da84c2593e1273
t05h19@naver.com <t05h19@naver.com>
2024-11-26 21:57:13 +0900

    fix: 보안 충돌 해결

--- src/main/java/com/example/fashioncommuni/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/example/fashioncommuni/security/filter/JwtAuthorizationFilter.java
@@ -50,7 +50,8 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                 "/user/register",
                 "/register",
                 "/chat/**",
-                "/chatRoom/**"
+                "/chatRoom/**",
+                "/post/**"
         );
 
         // 2. 토큰이 필요하지 않은 API URL의 경우 -> 로직 처리없이 다음 필터로 이동한다.
